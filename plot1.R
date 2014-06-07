d455feb611e5bf45a2c68c6f71c34ee13e4ff114
Manuel J. Castro <mjcastro@mjcastro.com>
2014-06-07 14:08:02 -0400

    Plot 1 finished, including export of png

--- plot1.R
+++ plot1.R
@@ -14,4 +14,12 @@ power$Date = as.Date(power$Date, format = "%d/%m/%Y")
 
 pwr = with(power, subset(power, Date == "2007-02-01" | Date == "2007-02-02"))
 
+
+png(filename = "plot1.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white", res = NA, family = "", restoreConsole = TRUE
+    )
+
 hist(pwr$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col = "red", ylim = c(0, 1200))
+
+dev.off()
